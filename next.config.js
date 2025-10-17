ecd0d2238d4dde4d2f745ece585cade222917b49
troyfry <tronicfrenz@gmail.com>
2025-10-17 15:43:36 -0400

    fix: valid next.config.js (remove shell text)

--- next.config.js
+++ next.config.js
@@ -1,8 +1,1 @@
-cat > next.config.js << 'EOF'
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true }
-};
-module.exports = nextConfig;
-EOF
+module.exports = {};
